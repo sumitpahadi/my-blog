5e9d8641e23de85909f9b307407474d86da5b997
sumitpahadi <sumitrawat1102000@gmail.com>
2023-08-01 13:54:17 +0530

    six commit

--- src/Component/Contextapi/Contextapi.js
+++ src/Component/Contextapi/Contextapi.js
@@ -13,6 +13,22 @@ import tujhootimaimakar from "../images/tujhootimaimakar.jpg"
 import Attack from "../images/attack.jpeg"
 import bachanpandey from "../images/bachanpandey.jpg"
 
+import bhuj from "../images/bhuj.jpg"
+import circus from "../images/circus.jpeg"
+import shamsherre from "../images/shamsherra.jpeg"
+import jabwemet from "../images/jabwemet.jpg"
+import brahmsastra from "../images/brahmsastra.jpg"
+import bhootpolice from "../images/bhootpolice.jpeg"
+import gangubhai from "../images/gangubhai.jpeg"
+
+
+import bhediya from "../images/bhediya.jpg"
+import dhakkad from "../images/dhakkad.jpg"
+import jersey from "../images/jersey.png"
+import vikrantrona from "../images/vikrantrona.jpg"
+import villian from "../images/villain.jpg"
+
+
 
 
 const Contextapi = createContext([
@@ -83,8 +99,129 @@ const Contextapi = createContext([
     id: 10,
     Image:bachanpandey ,
     heading: "Bachan pandey",
-    description: "Bachchan Pandey is a Bollywood action-comedy film featuring a popular superstar in the lead role. The story revolves around a rugged and eccentric character named Bachchan Pandey, who is known for his unique style and antics. Despite his carefree demeanor, Pandey finds himself entangled in a series of comical yet thrilling situations that lead him to unexpected adventures.Set against the backdrop of a small town, the film showcases Pandey's humorous encounters with the locals and his hilarious attempts to solve their problems. Along the way, he also confronts powerful adversaries and takes on various challenges with his trademark flair.",
+    description: "Bachchan Pandey is an action-comedy Bollywood movie starring Akshay Kumar in the lead role. The film revolves around the character of Bachchan Pandey, a small-time gangster and aspiring actor who gets entangled in a series of hilarious and adventurous situations. With a mix of comedy, drama, and thrilling action sequences, the movie follows Bachchan Pandey's journey as he tries to navigate the world of crime and showbiz. Filled with entertaining dialogues and high-octane action, Bachchan Pandey promises to be an entertaining ride for audiences of all ages. The film showcases Akshay Kumar's versatility as an actor and is sure to leave the audience entertained throughout.",
   },
+  {
+    id:11,
+    Image:bhuj,
+    heading:"BHUJ",
+    description:"Bhuj: The Pride of India is a Bollywood war drama based on true events that occurred during the 1971 Indo-Pakistani War. The film narrates the heroic tale of IAF Squadron Leader Vijay Karnik, played by Ajay Devgn, who was in charge of the Bhuj airbase in Gujarat during the war. When the base is destroyed in a surprise attack by Pakistani forces, Karnik and a team of 300 women from the local village work together to reconstruct the airbase and help the Indian Air Force launch crucial airstrikes. The movie showcases bravery, patriotism, and the indomitable spirit of the people during the war, making it a captivating and inspiring watch for audiences.",
+  },
+  {
+    id:12,
+    Image:circus,
+    heading:"Circus",
+    description:"Cirkus is a Bollywood comedy film directed by Rohit Shetty. The movie revolves around a series of hilarious events that occur when two identical twins, played by Ranveer Singh, find themselves in unexpected situations involving mistaken identity and confusion. As the story unfolds, the twins become entangled with various characters, including gangsters, goons, and a beautiful woman, portrayed by Jacqueline Fernandez. The film promises an entertaining and laughter-filled experience as the twins navigate through a roller-coaster of comical situations and attempt to resolve the chaos they inadvertently create. With Rohit Shetty's signature style of action-packed sequences and witty humor, Cirkus is sure to be a treat for comedy enthusiasts and Bollywood fans alike.",
+  },
+  {
+    id:13,
+    Image:shamsherre,
+    heading:"Shamshera",
+    description:"Shamshera is an action-adventure Bollywood film set in the 1800s. The story follows the journey of a young and courageous dacoit, played by Ranbir Kapoor, who rises to become a feared leader of a bandit group. Set against the backdrop of a kingdom torn by conflicts, the movie delves into themes of loyalty, bravery, and the struggle for justice. As the dacoit's path crosses with that of a ruthless ruler, portrayed by Sanjay Dutt, the movie escalates into a high-stakes battle between good and evil. With epic action sequences, powerful performances, and a gripping narrative, Shamshera promises to be a thrilling and captivating cinematic experience for audiences.",
+  },
+  {
+    id:14,
+    Image:jabwemet,
+    heading:"Jab we met",
+    description:"Jab We Met is a romantic Bollywood film that revolves around the journey of two contrasting individuals, Aditya and Geet, portrayed by Shahid Kapoor and Kareena Kapoor Khan, respectively. Aditya, a heartbroken businessman, meets Geet, an exuberant and carefree girl, on a train journey. Their paths intertwine, leading to a series of comedic and emotional encounters. As they travel together, they discover love, friendship, and self-discovery. The movie beautifully portrays the transformation of the characters and the power of love to heal wounds and bring happiness. With soulful music, heartwarming performances, and a heartwarming story, Jab We Met remains a beloved and timeless classic in Indian cinema.",
+  },
+  {
+    id:15,
+    Image:brahmsastra,
+    heading:"Brahmastra",
+    description:"Brahmastra is an upcoming Bollywood fantasy-adventure film directed by Ayan Mukerji. The movie features an ensemble cast, including Ranbir Kapoor, Alia Bhatt, Amitabh Bachchan, and Nagarjuna Akkineni. Set in a fictional world, the story follows Shiva, a young man with extraordinary powers who embarks on a quest to discover the ancient weapon Brahmastra that holds immense power. Alongside him is his love interest, Isha, who also possesses mystical abilities. As they journey together, they encounter mystical forces, epic battles, and uncover secrets that will shape the destiny of the world. The film promises a unique blend of mythology, action, and romance, making it one of the most anticipated movies in Indian cinema.",
+  },
+  {
+    id:16,
+    Image:bhootpolice,
+    heading:"Bhootploice",
+    description:"Bhoot Police is a horror-comedy Bollywood film directed by Pavan Kirpalani. The movie stars Saif Ali Khan, Arjun Kapoor, Jacqueline Fernandez, and Yami Gautam in lead roles. The story revolves around two ghost-hunting brothers, Vibhooti and Chiraunji, who are known for scamming people with their fake ghost encounters. However, their lives take a turn when they encounter real paranormal activities at a haunted mansion. As they delve deeper into the mystery, they team up with a mysterious woman, Kanika, and a local police officer, Rudra, to unravel the truth behind the spooky occurrences. Bhoot Police promises a thrilling and humorous ride, combining scares and laughs to create an entertaining cinematic experience for the audience.",
+  },
+  {
+    id:17,
+    Image:gangubhai,
+    heading:"Gangubai",
+    description:"Gangubai is a Bollywood crime drama film directed by Sanjay Leela Bhansali. The movie stars Alia Bhatt in the titular role, portraying the life of Gangubai Kothewali, a powerful and influential woman who was a notorious madam in the Kamathipura area of Mumbai. The story is based on a chapter from the book Mafia Queens of Mumbai by S. Hussain Zaidi. Gangubai's journey from being a young girl sold into the prostitution business to becoming a feared and respected figure in the underworld is depicted in the film. Known for her fearless and determined personality, Gangubai was also known for her philanthropic work in the community. The film offers a gripping narrative of her life, showcasing both her struggles and triumphs, making it an intriguing and compelling cinematic experience.",
+  },
+  {
+    id:18,
+    Image:bhediya,
+    heading:"Bhediya",
+    description:"Bhediya is an Indian horror-comedy film directed by Amar Kaushik. The movie stars Varun Dhawan and Kriti Sanon in lead roles. Set in a small town, the story revolves around the myth of werewolves, where strange and mysterious events start occurring. Varun Dhawan's character is bitten by a werewolf, leading to a series of comedic and thrilling situations as he discovers his newfound powers and struggles to control them. Kriti Sanon plays an important role as she supports him in his journey. The film blends elements of horror, comedy, and romance, making it an entertaining and unique cinematic experience. Bhediya promises a roller-coaster ride of emotions, laughter, and scares, making it a must-watch for fans of the genre.",
+  },
+  {
+    id:19,
+    Image:dhakkad,
+    heading:"Dhakkad",
+    description:"Dhakkad is a high-octane action film set in the backdrop of a small town. The movie features a strong and fearless female protagonist, portrayed by a leading Bollywood actress. She takes on the role of a fierce and determined young woman who seeks justice for her family and community. The film delves into themes of empowerment, courage, and resilience as the protagonist confronts powerful adversaries and fights against all odds. With intense action sequences, gripping storytelling, and a powerful message, Dhakkad promises to be a thrilling and empowering cinematic experience that celebrates the strength and bravery of women",
+  },
+  {
+    id:20,
+    Image:jersey,
+    heading:"Jersey",
+    description:"Jersey is a heartwarming and inspiring sports drama that revolves around the life of a talented cricketer. The film follows the protagonist, portrayed by a renowned actor, who faces numerous challenges and setbacks in his cricketing career. Despite the odds, he decides to make a remarkable comeback to the sport, seeking redemption and proving his worth as a player and a father. Jersey beautifully captures the emotions of determination, love, and perseverance, making it a touching and relatable story for audiences. With brilliant performances, soul-stirring moments, and a powerful narrative, Jersey stands as a testament to the undying spirit of chasing one's dreams.",
+  },
+  {
+    id:21,
+    Image:vikrantrona,
+    heading:"Vikrant Rona",
+    description:"Vikrant Rona is a 2022 Indian Kannada-language action thriller film written and directed by Anup Bhandari, produced by Jack Manjunath and co-produced by Alankar Pandian. It stars Sudeepa in the title role with Nirup Bhandari, Neetha Ashok, and Jacqueline Fernandez. The story revolves around Inspector Vikrant Rona, who arrives at a remote village in the middle of a tropical rainforest where he starts witnessing a series of unexplainable events which are attributed to the supernatural. Vikrant Rona was released theatrically in 3D on 28 July 2022, and digitally in ZEE5 on 2 September 2022. The film received mixed to negative reviews from critics but received positive response from audience and thus became the third highest-grossing Kannada film at the time of its release.",
+  },
+  {
+    id:22,
+    Image:villian,
+    heading:"Ek villian Return",
+    description:"Ek Villain Returns is a 2022 Bollywood action-thriller film, directed by Mohit Suri and produced by Ekta Kapoor. The movie stars John Abraham, Arjun Kapoor, Disha Patani and Tara Sutaria in the lead roles. Ek Villain Returns depicts the story of two men in one sided love. The movie released in theatres on 29 July 2022. Ek Villain Returns is produced by Shobha Kapoor, Ekta Kapoor, Bhushan Kumar and Krishan Kumar under the banner T-Series and Balaji Motion Pictures. The movie has its screenplay penned by Mohit Suri and Aseem Arora. Vikas Sivaraman has wielded the camera for Ek Villain Returns. Aditya Roy Kapur was originally signed for the role portrayed by Arjun Kapoor, but later he stepped back.",
+  },
+  {
+    id:23,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:24,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:25,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:26,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:27,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:28,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:29,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+  {
+    id:30,
+    Image:"",
+    heading:"",
+    description:"",
+  },
+
  
 
 ]);
--- src/Component/pages/Bollywood.js
+++ src/Component/pages/Bollywood.js
@@ -1,17 +1,32 @@
 import React, { useContext } from "react";
 import Contextapi from "../Contextapi/Contextapi";
 import { Link } from "react-router-dom";
+
 import "./Style.css";
 
 export const Bollywood = () => {
   const bollywooddata = useContext(Contextapi);
   console.log(bollywooddata);
 
+  function truncateDescription(description, numWords) {
+    const words = description.split(" ");
+    const truncatedWords = words.slice(0, numWords);
+    const truncatedDescription = truncatedWords.join(" ");
+    return truncatedDescription;
+  }
+
+  // ------------------------------------------usesate---------------------------
+
+ 
+
   return (
     <>
+      <div className="boll-heading">
+        <h1>Bollywood</h1>
+      </div>
       <div className="bollywood-data">
         {bollywooddata
-          .filter((item) => item.id > 0)
+          .filter((item) => item.id > 0 && item.id < 9)
           .map((data, index) => {
             return (
               <div className="imagedata" key={index}>
@@ -19,12 +34,81 @@ export const Bollywood = () => {
                   <Link to={"/detaildescription/" + data.id}>
                     <img src={data.Image} alt="click here" />
                   </Link>
+                  <div className="title">{data.heading}</div>
 
-                  <div className="description">{data.description}</div>
+                  <div className="description">
+                    {truncateDescription(data.description, 50)}
+                  </div>
                 </div>
               </div>
             );
           })}
+      </div>
+
+      {/* --------------top stories------------------------ */}
+      <div className="stories">Top stories</div>
+
+      <div className="bollywood-first-container">
+        <div className="bollywood-firstinner-container">
+          <div className="bollywood-content">
+            {bollywooddata
+              .filter((item) => item.id >= 10 && item.id < 19)
+              .map((data, index) => {
+                return (
+                  <>
+                    <div className="child" key={index}>
+                      <Link to={"/detaildescription/" + data.id}>
+                        <img src={data.Image} alt="click here" />
+                      </Link>
+                      <div className="title">{data.heading}</div>
+
+                      <div className="description">
+                        {truncateDescription(data.description, 30)}
+                      </div>
+                    </div>
+                    <hr />
+                  </>
+                );
+              })}
+            <div className="load">
+              {" "}
+              <span class="material-symbols-outlined">
+                keyboard_double_arrow_down
+              </span>
+              LOAD MORE{" "}
+            </div>
+          </div>
+        </div>
+
+        <div className="bollywood-secondinner-container">
+          <div className="bollywood-inner-content">
+            <div className="post">Top Post</div>
+            {bollywooddata
+              .filter((item) => item.id >= 19 && item.id < 23)
+              .map((data, index) => {
+                return (
+                  <>
+                    <div className="child" key={index}>
+                      <Link to={"/detaildescription/" + data.id}>
+                        <img src={data.Image} alt="click here" />
+                      </Link>
+                      <div className="title">{data.heading}</div>
+
+                      <div className="description">
+                        {truncateDescription(data.description, 10)}
+                      </div>
+                      <div className="count">1</div>
+                    </div>
+
+                    <hr />
+                  </>
+                );
+              })}
+            <div className="advertisement">
+              <p>advertisement</p>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
